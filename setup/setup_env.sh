ebeb27696928e75c9d7548d24603ca838f525df9
jakebentley2001 <jakebentley2001@gmail.com>
2025-09-07 18:16:07 -0400

    more change config

--- setup/setup_env.sh
+++ setup/setup_env.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=upload_model
-#SBATCH --output=./logs/upload_model_%j.out
-#SBATCH --error=./logs/upload_model_%j.err
-#SBATCH --time=01:00:00
+#SBATCH --job-name=setup_env
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=8G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:A100_40GB:1
 #SBATCH --partition=general
 
 #Load conda
